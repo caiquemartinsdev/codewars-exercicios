a9164165183545ee6e26dbefafd510972a347b16
Caique Martins <caiquemartins.dev@gmail.com>
2022-09-27 13:54:22 -0300

    exercício 1

---
+++ script.js
@@ -0,0 +1,29 @@
+// Exercício1
+
+// Trolls estão atacando sua seção de comentários!
+// Uma forma comum de lidar com essa situação é remover todas as vogais dos comentários dos trolls, neutralizando a ameaça.
+// Sua tarefa é escrever uma função que receba uma string e retorne uma nova string com todas as vogais removidas.
+// Por exemplo, a string "Este site é para perdedores LOL!" se tornaria "Ths wbst s fr lsrs LL!".
+
+// const entradaFrase = (callback) =>{
+//     let frase = prompt('Digite uma frase:')
+//     callback(frase)
+// }
+
+// const removerVogal = (frase) => alert(frase.replace(/[aeiou]/gi,''))
+
+// entradaFrase(removerVogal)
+// ----------------------------
+
+// EXERCÍCIO 2
+
+
+
+
+
+
+
+
+
+
+
