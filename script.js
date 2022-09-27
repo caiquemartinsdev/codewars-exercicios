5bc3d373b0ca143a8a8895b9160a2e892b36345f
Caique Martins <caiquemartins.dev@gmail.com>
2022-09-27 14:42:05 -0300

    exercício 2

--- script.js
+++ script.js
@@ -16,9 +16,17 @@
 // ----------------------------
 
 // EXERCÍCIO 2
+// Neste kata, você criará uma função que recebe uma lista de inteiros e strings
+//  não negativos e retorna uma nova lista com as strings filtradas.
 
+//Exemplo 
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
-
+function filter_list(l) {
+    return l.filter(item => typeof item === 'number');
+  }
 
 
 
